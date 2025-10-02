df2958eeb5c3b517dd3dd1c9fc3697fdc160bcec
v1nvn <v1n@outlook.com>
2025-10-02 11:18:00 +0530

    fix: version info

--- cmd/zaplint/main.go
+++ cmd/zaplint/main.go
@@ -24,22 +24,54 @@ type versionFlag struct{}
 func (versionFlag) String() string   { return "" }
 func (versionFlag) IsBoolFlag() bool { return true }
 func (versionFlag) Set(string) error {
-	v := getVersion()
+	v, vcsInfo := getVersion()
 	fmt.Printf("zaplint version %s %s/%s (built with %s)\n", v, runtime.GOOS, runtime.GOARCH, runtime.Version())
+	if vcsInfo != "" {
+		fmt.Println(vcsInfo)
+	}
 	os.Exit(0)
 	return nil
 }
 
-func getVersion() string {
+func getVersion() (string, string) {
+	var vcsInfo string
+
 	if version != "dev" {
-		return version
+		return version, ""
 	}
 
 	if info, ok := debug.ReadBuildInfo(); ok {
 		if info.Main.Version != "" && info.Main.Version != "(devel)" {
-			return info.Main.Version
+			version = info.Main.Version
+		} else {
+			version = "dev"
 		}
+
+		// Extract VCS information
+		var revision, time, modified string
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "vcs.revision":
+				revision = setting.Value
+			case "vcs.time":
+				time = setting.Value
+			case "vcs.modified":
+				modified = setting.Value
+			}
+		}
+
+		if revision != "" {
+			vcsInfo = fmt.Sprintf("commit: %s", revision)
+			if time != "" {
+				vcsInfo += fmt.Sprintf(", built at: %s", time)
+			}
+			if modified == "true" {
+				vcsInfo += " (dirty)"
+			}
+		}
+
+		return version, vcsInfo
 	}
 
-	return "dev"
+	return "dev", ""
 }
